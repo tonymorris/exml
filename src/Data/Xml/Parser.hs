0b314ae0146199198b37a31e42598ade3e7f75fa
Tony Morris <tmorris@tmorris.net>
2014-11-24 16:09:07 +1000

    parser

--- src/Data/Xml/Parser.hs
+++ src/Data/Xml/Parser.hs
@@ -5,11 +5,14 @@
 module Data.Xml.Parser where
 
 import Control.Applicative
-import Control.Lens(Optic, Choice, prism)
-import Control.Monad(ap)
-import Data.Bifunctor
-import Data.Functor.Apply
-import Data.Functor.Bind
+import Control.Lens(Optic, Choice, prism, set, over, (^?))
+import Data.Bifunctor(Bifunctor(bimap))
+import Data.Bool(bool)
+import Data.Digit(Digit, digitC)
+import Data.Functor.Alt(Alt((<!>)))
+import Data.Functor.Apply(Apply((<.>)))
+import Data.Functor.Bind(Bind((>>-)))
+import Data.List.NonEmpty(NonEmpty((:|)), toList)
 import Prelude
 
 type Input =
@@ -18,7 +21,7 @@ type Input =
 data Result e a =
   ErrorResult e
   | ValueResult Input a
-  deriving (Eq, Ord)  
+  deriving (Eq, Ord, Show)  
 
 instance Functor (Result e) where
   fmap _ (ErrorResult e) =
@@ -60,45 +63,145 @@ instance (Choice p, Applicative f) => AsValueResult p f Result where
                ValueResult s a ->
                  Right (s, a))
 
-data ResultT e f a =
-  ResultT
-    (f (Result e a))
+newtype Parser s e a =
+  Parser (Input -> s -> (s, Result e a))
 
-instance Functor f => Functor (ResultT e f) where
-  fmap f (ResultT x) =
-    ResultT (fmap (fmap f) x)
-
-newtype Parser e f a =
-  Parser
-    (Input -> ResultT e f a)
-
-instance (Functor f, Monad f) => Functor (Parser e f) where
+instance Functor (Parser s e) where
   fmap f (Parser p) =
-    Parser (fmap f . p)
+    Parser (\i s -> 
+      let (t, r) = p i s
+      in (t, fmap f r))
 
-instance (Functor f, Monad f) => Applicative (Parser e f) where
-  pure =
-    return
+instance Apply (Parser s e) where
+  Parser f <.> Parser a =
+    Parser (\i s -> 
+      let (t, r) = f i s          
+      in case r of 
+           ErrorResult e ->
+             (t, ErrorResult e)
+           ValueResult j g ->
+             fmap (fmap (g$)) (a j t))
+
+instance Applicative (Parser s e) where
   (<*>) =
-    ap
+    (<.>)
+  pure a =
+    Parser (\i s ->
+      (s, ValueResult i a))
 
-instance (Functor f, Monad f) => Apply (Parser e f) where
-  (<.>) =
-    (<*>)
+instance Bind (Parser s e) where
+  Parser p >>- f =
+    Parser (\i s -> 
+      let (t, r) = p i s          
+      in case r of 
+           ErrorResult e ->
+             (t, ErrorResult e)
+           ValueResult j a ->
+             let Parser q = f a 
+             in q j t)
 
-instance (Functor f, Monad f) => Bind (Parser e f) where
-  (>>-) =
-    (>>=)
+instance Monad (Parser s e) where
+  (>>=) =
+    (>>-)
+  return =
+    pure
 
-instance (Functor f, Monad f) => Monad (Parser e f) where
-  return a =
-    Parser (\i -> ResultT . return . ValueResult i $ a)
-  Parser p >>= f =
-    Parser (\i -> let ResultT r = p i
-                  in ResultT (r >>= \s -> case s of
-                                            ErrorResult e ->
-                                              return (ErrorResult e)
-                                            ValueResult j a ->
-                                              let Parser q = f a
-                                                  ResultT t = q j
-                                              in t))
+instance Alt (Parser s e) where
+  Parser p <!> Parser q =
+    Parser (\i s -> 
+      let (t, r) = p i s          
+      in case r of 
+           ErrorResult _ ->
+             q i s
+           ValueResult j a ->
+             (t, ValueResult j a))
+
+(.=.) ::
+  e
+  -> Parser s x a
+  -> Parser s e a
+e .=. Parser p =
+  Parser (\i ->
+    fmap (set _ErrorResult e) . p i)
+  
+infixl 6 .=.
+
+(.~.) ::
+  (x -> e)
+  -> Parser s x a
+  -> Parser s e a
+e .~. Parser p =
+  Parser (\i ->
+    fmap (over _ErrorResult e) . p i)
+
+infixl 6 .~.
+
+class SemiCharState s where
+  updateCharState ::
+    Char
+    -> s
+    -> s
+
+class SemiCharState s => CharState s where
+  emptyCharState ::
+    s
+  
+failed ::
+  e
+  -> Parser s e a
+failed e =
+  Parser (\_ s -> 
+    (s, ErrorResult e))
+
+character ::
+  SemiCharState s =>
+  Parser s () Char
+character =
+  Parser (\i s -> 
+    case i of
+      [] ->
+        (s, ErrorResult ())
+      h:t ->
+        (updateCharState h s, ValueResult t h))
+
+list ::
+  Parser s e a
+  -> Parser s e [a]
+list p =
+  fmap toList (list1 p) <!> pure []  
+
+
+list1 ::
+  Parser s e a
+  -> Parser s e (NonEmpty a)
+list1 p =
+  do h <- p
+     t <- list p
+     return (h :| t)
+
+satisfy ::
+  SemiCharState s =>
+  (Char -> Bool)
+  -> Parser s Bool Char
+satisfy f =
+  do c <- True .=. character
+     bool (failed False) (return c) (f c)
+
+is ::
+  SemiCharState s =>
+  Char
+  -> Parser s Bool Char
+is c =
+  satisfy (== c)
+
+digit ::
+  SemiCharState s =>
+  Parser s Bool Digit
+digit =
+  do c <- True .=. character
+     case c ^? digitC of
+       Nothing ->
+         failed False
+       Just d ->
+         return d
+  
